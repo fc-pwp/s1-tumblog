0778068a80ccf3b8a6b942cc225ae29c31cea903
Kay Cha <kay@hannal.net>
2016-03-02 23:07:45 +0900

    add paginator

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.core.paginator import PageNotAnInteger
+from django.core.paginator import EmptyPage
 
 from blog.models import Post
 
 
 def list_posts(request):
+    page = request.GET.get('page', 1)
+
     object_list = Post.objects.all().order_by('-created_at')
+    per_page = 2
+    pn = Paginator(object_list, per_page)
+
+    try:
+        posts = pn.page(page)
+    except PageNotAnInteger:
+        posts = pn.page(1)
+    except EmptyPage:
+        posts = pn.page(pn.num_pages)
+
     ctx = {
-        'posts': object_list,
+        'posts': posts,
     }
     return render(request, 'post_list.html', ctx)
 
 
 def view_post(request, pk):
-    the_post = Post.objects.get(pk=pk)
+    the_post = get_object_or_404(Post, pk=pk)
     ctx = {
         'post': the_post,
     }
--- tumblog/urls.py
+++ tumblog/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
 from django.contrib import admin
 
-from blog.views import list_posts
-from blog.views import view_post
+from blog import views as blog_views
 
 
 urlpatterns = [
-    url(r'^blog/posts/(?P<pk>[0-9]+)/$', view_post, name='view_post'),
-    url(r'^blog/$', list_posts, name='list_posts'),
+    url(r'^blog/posts/(?P<pk>[0-9]+)/$', blog_views.view_post, name='view_post'),
+    url(r'^blog/$', blog_views.list_posts, name='list_posts'),
     url(r'^admin/', admin.site.urls),
 ]
 

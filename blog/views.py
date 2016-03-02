6c4f930bae6be66624a961f919915a00bb4921b2
Kay Cha <kay@hannal.net>
2016-03-02 20:45:43 +0900

    add list_posts() view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def list_posts(request):
+    text = '''
+    <strong>화면 떴다</strong>
+
+    <p>이상한 html 문서 구조이지만. 됨</p>
+    '''
+    return HttpResponse(text)
+

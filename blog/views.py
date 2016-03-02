e75f045dc19383b57ee37930ec8ffbefa78d410f
Kay Cha <kay@hannal.net>
2016-03-02 22:15:12 +0900

    add view_post view

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blog.models import Post
+
 
 def list_posts(request):
-    text = '''
-    <strong>화면 떴다</strong>
+    object_list = Post.objects.all().order_by('-created_at')
+    ctx = {
+        'posts': object_list,
+    }
+    return render(request, 'post_list.html', ctx)
 
-    <p>이상한 html 문서 구조이지만. 됨</p>
-    '''
-    return HttpResponse(text)
+
+def view_post(request, pk):
+    the_post = Post.objects.get(pk=pk)
+    ctx = {
+        'post': the_post,
+    }
+    return render(request, 'post_view.html', ctx)
 
--- tumblog/urls.py
+++ tumblog/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from blog.views import list_posts
+from blog.views import view_post
 
 
 urlpatterns = [
+    url(r'^blog/posts/(?P<pk>[0-9]+)/$', view_post, name='view_post'),
     url(r'^blog/$', list_posts, name='list_posts'),
     url(r'^admin/', admin.site.urls),
 ]

41dfaf1dbaf13326f2d7d6fa7ccb82a6cfe8fcc5
Kay Cha <kay@hannal.net>
2016-03-05 11:32:49 +0900

    add save_post feature

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from django.core.paginator import Paginator
@@ -6,6 +7,7 @@ from django.core.paginator import PageNotAnInteger
 from django.core.paginator import EmptyPage
 
 from blog.models import Post
+from blog.models import Category
 
 
 def list_posts(request):
@@ -37,5 +39,33 @@ def view_post(request, pk):
 
 
 def create_post(request):
-    return render(request, 'edit_post.html')
+    if request.user.is_authenticated() is False:
+        raise Exception('로그인 하세요')
+
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        category_pk = request.POST.get('category')
+
+        if not title or not category_pk or not content:
+            raise Exception('제목과 카테고리와 본문은 필수')
+
+        category = get_object_or_404(Category, pk=category_pk)
+        new_post = Post()
+        new_post.title = title
+        new_post.content = content
+        new_post.category = category
+        new_post.user = request.user
+        new_post.save()
+
+        return redirect('view_post', pk=new_post.pk)
+
+    elif request.method == 'GET':
+        categories = Category.objects.all()
+
+    ctx = {
+        'categories': categories,
+    }
+
+    return render(request, 'edit_post.html', ctx)
 

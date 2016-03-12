c4e4bebee253d5e72d9a24b2aa73170660eecc3a
Kay Cha <kay@hannal.net>
2016-03-12 09:17:24 +0900

    add create_comment url

--- tumblog/urls.py
+++ tumblog/urls.py
@@ -5,6 +5,10 @@ from blog import views as blog_views
 
 
 urlpatterns = [
+    url(
+        r'^blog/posts/(?P<pk>[0-9]+)/create_comment/$',
+        blog_views.create_comment, name="create_comment"
+    ),
     url(r'^blog/posts/edit/$', blog_views.create_post, name='create_post'),
     url(r'^blog/posts/(?P<pk>[0-9]+)/$', blog_views.view_post, name='view_post'),
     url(r'^blog/$', blog_views.list_posts, name='list_posts'),
